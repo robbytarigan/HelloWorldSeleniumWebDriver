73dc8249d91ab736bd662256d99987dc7ed38ae5
Robby Tarigan <robby.tarigan@gmail.com>
2011-07-28 22:13:32 +0100

    Use Newtonsoft.Json.Net35.dll and assert driver.Title.

--- src/HelloWorldSeleniumWebDriver/UnitTest1.cs
+++ src/HelloWorldSeleniumWebDriver/UnitTest1.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿namespace HelloWorldSeleniumWebDriver {
+    #region Using
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.IE;
 
-namespace HelloWorldSeleniumWebDriver {
+    #endregion Using
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
         public void TestMethod1() {
+            // Create a new WebDriver instance
+            IWebDriver driver = new InternetExplorerDriver();
+
+            // Go to Bing.com
+            driver.Url = "http://www.bing.com";
+
+            // Find the search box and enter a search
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("selenium");
+            driver.FindElement(By.Name("go")).Click();
+
+            Assert.IsTrue(driver.Title.Contains("Bing"));
+
+            driver.Close();
         }
     }
 }
